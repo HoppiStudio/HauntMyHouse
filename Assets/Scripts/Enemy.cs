b01263433aa00f411ba7c11fc335ecc7ef9b9d9c
Charlie <charlie6.evans@live.uwe.ac.uk>
2022-12-07 00:10:43 +0000

    Started laying the groundwork for the enemy classes

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+    [SerializeField] private EnemyData enemyData;
+    private StateMachine _stateMachine = new StateMachine();
+    private Dictionary<Type, IState> _cachedStatesDict;
+
+    private void Awake()
+    {
+        _cachedStatesDict = new Dictionary<Type, IState>
+        {
+            {typeof(EnemyIdleState), new EnemyIdleState(this, _stateMachine)},
+        };
+        _stateMachine.SetStates(_cachedStatesDict);
+        _stateMachine.ChangeState(typeof(EnemyIdleState));
+    }
+
+    private void Update()
+    {
+        _stateMachine.Update();
+    }
+}
---
+++ Assets/Scripts/EnemyData.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewEnemyData", menuName = "Scriptables/EnemyData", order = 1)]
+public class EnemyData : ScriptableObject
+{
+    public string name;
+    public int health;
+    public GameObject prefab;
+}
