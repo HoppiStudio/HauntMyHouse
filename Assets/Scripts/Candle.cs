e3a6621e28fc5f0c8c54bd7687d4f19ac4ba4193
Charlie <charlie6.evans@live.uwe.ac.uk>
2023-01-25 04:50:05 +0000

    Added matchstick and candle flame colour swapping

--- Assets/Scripts/Candle.cs
+++ Assets/Scripts/Candle.cs
@@ -23,8 +23,8 @@ public class Candle : MonoBehaviour, IFlammable
     }
     public FlameColour FlameColour
     {
-        get => flameColour; 
-        set{}
+        get => flameColour;
+        set => flameColour = value;
     }
     public bool IsOnFire { get; set; }
     
@@ -36,37 +36,7 @@ public class Candle : MonoBehaviour, IFlammable
 
     private void OnValidate()
     {
-        switch (flameColour)
-        {
-            case FlameColour.White:
-                firePS.startColor = Color.white;
-                lightSource.color = Color.white;
-                break;
-            case FlameColour.Red:
-                firePS.startColor = Color.red;
-                lightSource.color = Color.red;
-                break;
-            case FlameColour.Green:
-                firePS.startColor = Color.green;
-                lightSource.color = Color.green;
-                break;
-            case FlameColour.Blue:
-                firePS.startColor = Color.blue;
-                lightSource.color = Color.blue;
-                break;
-            case FlameColour.Orange:
-                firePS.startColor = new Color(1,0.5f,0);
-                lightSource.color = new Color(1,0.5f,0);
-                break;
-            case FlameColour.Purple:
-                firePS.startColor = new Color(0.5f, 0, 1);
-                lightSource.color = new Color(0.5f, 0, 1);
-                break;
-            default:
-                firePS.startColor = Color.white;
-                lightSource.color = Color.white;
-                break;
-        }
+        CheckFlameColour();
     }
 
     private void Start()
@@ -82,10 +52,19 @@ public class Candle : MonoBehaviour, IFlammable
     {
         if (other.GetComponent<IFlammable>() != null)
         {
-            // If this object is on fire, ignite other flammable objects (if not already on fire themselves)
+            // If this object is on fire, ignite other flammable objects (if they aren't already on fire)
             if (!other.GetComponent<IFlammable>().IsOnFire && IsOnFire)
             {
                 var flammable = other.GetComponent<IFlammable>();
+                flammable.FlameColour = flameColour;
+                flammable.Ignite();
+            }
+
+            // If this candle is on fire, change flame colour of matchstick even if it's on fire already
+            if (other.GetComponent<Matchstick>() != null && IsOnFire)
+            {
+                var flammable = other.GetComponent<IFlammable>();
+                flammable.FlameColour = flameColour;
                 flammable.Ignite();
             }
         }
@@ -93,6 +72,7 @@ public class Candle : MonoBehaviour, IFlammable
 
     public void Ignite() // Abstract class would probably be more ideal
     {
+        CheckFlameColour();
         FirePS.Play();
         LightSource.enabled = true;
         LightVolumes.ForEach(ctx => ctx.SetActive(true));
@@ -109,5 +89,40 @@ public class Candle : MonoBehaviour, IFlammable
         IsOnFire = false;
         BanishManager.Instance.OnCandleUnlitEvent(); // TODO: Tidy this
         //OnCandleUnlit?.Invoke();
+    }
+    
+    private void CheckFlameColour()
+    {
+        switch (flameColour)
+        {
+            case FlameColour.White:
+                firePS.startColor = Color.white;
+                lightSource.color = Color.white;
+                break;
+            case FlameColour.Red:
+                firePS.startColor = Color.red;
+                lightSource.color = Color.red;
+                break;
+            case FlameColour.Green:
+                firePS.startColor = Color.green;
+                lightSource.color = Color.green;
+                break;
+            case FlameColour.Blue:
+                firePS.startColor = Color.cyan;
+                lightSource.color = Color.cyan;
+                break;
+            case FlameColour.Orange:
+                firePS.startColor = new Color(1, 0.5f, 0);
+                lightSource.color = new Color(1, 0.5f, 0);
+                break;
+            case FlameColour.Purple:
+                firePS.startColor = new Color(0.5f, 0, 1);
+                lightSource.color = new Color(0.5f, 0, 1);
+                break;
+            default:
+                firePS.startColor = Color.white;
+                lightSource.color = Color.white;
+                break;
+        }
     }
 }
--- Assets/Scripts/Matchstick.cs
+++ Assets/Scripts/Matchstick.cs
@@ -19,7 +19,11 @@ public class Matchstick : MonoBehaviour, IFlammable
         get => lightVolumes;
         set {}
     }
-    public FlameColour FlameColour { get; set; }
+    public FlameColour FlameColour
+    {
+        get => flameColour;
+        set => flameColour = value;
+    }
     public bool IsOnFire { get; set; }
     
     [SerializeField] private ParticleSystem firePS;
@@ -32,37 +36,7 @@ public class Matchstick : MonoBehaviour, IFlammable
     
     private void OnValidate()
     {
-        switch (flameColour)
-        {
-            case FlameColour.White:
-                firePS.startColor = Color.white;
-                lightSource.color = Color.white;
-                break;
-            case FlameColour.Red:
-                firePS.startColor = Color.red;
-                lightSource.color = Color.red;
-                break;
-            case FlameColour.Green:
-                firePS.startColor = Color.green;
-                lightSource.color = Color.green;
-                break;
-            case FlameColour.Blue:
-                firePS.startColor = Color.blue;
-                lightSource.color = Color.blue;
-                break;
-            case FlameColour.Orange:
-                firePS.startColor = new Color(1,0.5f,0);
-                lightSource.color = new Color(1,0.5f,0);
-                break;
-            case FlameColour.Purple:
-                firePS.startColor = new Color(0.5f, 0, 1);
-                lightSource.color = new Color(0.5f, 0, 1);
-                break;
-            default:
-                firePS.startColor = Color.white;
-                lightSource.color = Color.white;
-                break;
-        }
+        CheckFlameColour();
     }
 
     private void Start()
@@ -93,10 +67,11 @@ public class Matchstick : MonoBehaviour, IFlammable
         
         if (other.GetComponent<IFlammable>() != null)
         {
-            // If this object is on fire, ignite other flammable objects (if not already on fire themselves)
+            // If this object is on fire, ignite other flammable objects (if they aren't already on fire)
             if (!other.GetComponent<IFlammable>().IsOnFire && IsOnFire)
             {
                 var flammable = other.GetComponent<IFlammable>();
+                flammable.FlameColour = flameColour;
                 flammable.Ignite();
             }
         }
@@ -113,6 +88,7 @@ public class Matchstick : MonoBehaviour, IFlammable
 
     public void Ignite()
     {
+        CheckFlameColour();
         FirePS.Play();
         LightSource.enabled = true;
         LightVolumes.ForEach(ctx => ctx.SetActive(true));
@@ -125,5 +101,40 @@ public class Matchstick : MonoBehaviour, IFlammable
         LightSource.enabled = false;
         LightVolumes.ForEach(ctx => ctx.SetActive(false));
         IsOnFire = false;
+    }
+    
+    private void CheckFlameColour()
+    {
+        switch (flameColour)
+        {
+            case FlameColour.White:
+                firePS.startColor = Color.white;
+                lightSource.color = Color.white;
+                break;
+            case FlameColour.Red:
+                firePS.startColor = Color.red;
+                lightSource.color = Color.red;
+                break;
+            case FlameColour.Green:
+                firePS.startColor = Color.green;
+                lightSource.color = Color.green;
+                break;
+            case FlameColour.Blue:
+                firePS.startColor = Color.cyan;
+                lightSource.color = Color.cyan;
+                break;
+            case FlameColour.Orange:
+                firePS.startColor = new Color(1, 0.5f, 0);
+                lightSource.color = new Color(1, 0.5f, 0);
+                break;
+            case FlameColour.Purple:
+                firePS.startColor = new Color(0.5f, 0, 1);
+                lightSource.color = new Color(0.5f, 0, 1);
+                break;
+            default:
+                firePS.startColor = Color.white;
+                lightSource.color = Color.white;
+                break;
+        }
     }
 }
