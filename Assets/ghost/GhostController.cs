0a7efb64278f0117729fe30d990f7ab453a14897
Charlie <charlie6.evans@live.uwe.ac.uk>
2023-02-01 00:43:14 +0000

    Ghosts now move towards player at eye-level | Added ghost body

--- Assets/ghost/GhostController.cs
+++ Assets/ghost/GhostController.cs
@@ -1,4 +1,3 @@
-using AiDirector.Scripts;
 using System;
 using System.Collections;
 using UnityEngine;
@@ -13,7 +12,7 @@ public enum GhostState
 public class GhostController : MonoBehaviour
 {
     [SerializeField] public GhostState state;
-    [SerializeField] private GameObject target;
+    [SerializeField] private Transform target;
     [SerializeField] private float speed = 10f;
 
     [SerializeField] private AudioClip Attack_Voice;
@@ -21,19 +20,21 @@ public class GhostController : MonoBehaviour
     [SerializeField] private AudioClip Normal_Voice;
 
     [NonSerialized] private Renderer Renderer;
+    private Vector3 _targetPos;
+    private int counter = 0;
 
     private void Start()
     {
- 
-
-        target = GameObject.Find("PlayerController");
-        Renderer = this.GetComponent<Renderer>();
+        target = GameObject.Find("PlayerController").transform;
+        _targetPos = target.position + Vector3.down*0.80f;
+        Renderer = GetComponent<Renderer>();
     }
-
-    private int counter = 0;
+    
     void Update()
     {
-        //this.transform.LookAt(target.transform.position, Vector3.up);
+        Debug.DrawLine(transform.position, _targetPos, Color.green);
+        
+        //transform.LookAt(_targetPos, Vector3.up);
         int randomized_int = UnityEngine.Random.Range(-2, 2);
         int state_randomizer = UnityEngine.Random.Range(0, 5);
         int sound_randomizer = UnityEngine.Random.Range(0, 2);
@@ -42,64 +43,60 @@ public class GhostController : MonoBehaviour
         {
             if(state_randomizer < 3)
             {
-                transform.position = Vector3.MoveTowards(this.transform.position, Randomized_Start_Location(), speed / 10 * Time.deltaTime * randomized_int);
-
-
-                if (this.GetComponent<AudioSource>().isPlaying == false && sound_randomizer == 1)
+                transform.position = Vector3.MoveTowards(transform.position, Randomized_Start_Location(), speed / 10 * Time.deltaTime * randomized_int);
+                
+                if (GetComponent<AudioSource>().isPlaying == false && sound_randomizer == 1)
                 {
-                    this.GetComponent<AudioSource>().PlayOneShot(Normal_Voice);
+                    GetComponent<AudioSource>().PlayOneShot(Normal_Voice);
                 }
                 Renderer.material.SetColor("_Color", Color.green);
             }
             else
             {
                 int movement_randomizer_towards = UnityEngine.Random.Range(-2, +1);
-                transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed / 10 * Time.deltaTime * movement_randomizer_towards);
+                transform.position = Vector3.MoveTowards(transform.position, _targetPos, speed / 10 * Time.deltaTime * movement_randomizer_towards);
             }
-          
         }
         else if(state == GhostState.Aggresive)
         {
             if (state_randomizer < 3)
             {
-                transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed / 10 * Time.deltaTime * 5f);
+                transform.position = Vector3.MoveTowards(transform.position, _targetPos, speed / 10 * Time.deltaTime * 5f);
                 if (counter ==0 && sound_randomizer == 1)
                 {
                     counter++;
-                    this.GetComponent<AudioSource>().PlayOneShot(Attack_Voice);
+                    GetComponent<AudioSource>().PlayOneShot(Attack_Voice);
                 }
                 Renderer.material.SetColor("_Color", Color.red);
             }
             else
             {
                 int randomized_int_2 = UnityEngine.Random.Range(-3, 3);
-                transform.RotateAround(target.transform.position, Vector3.up, speed * Time.deltaTime * randomized_int_2);
+                transform.RotateAround(_targetPos, Vector3.up, speed * Time.deltaTime * randomized_int_2);
             }
-
 
             StartCoroutine(Rotate_Around_Randomly());   
         }
         else if(state == GhostState.Scared)
         {
             counter = 0;
-            if (this.GetComponent<AudioSource>().isPlaying == false && sound_randomizer == 1)
+            if (GetComponent<AudioSource>().isPlaying == false && sound_randomizer == 1)
             {
-                this.GetComponent<AudioSource>().PlayOneShot(Stunned_Voice);
+                GetComponent<AudioSource>().PlayOneShot(Stunned_Voice);
             }
             Renderer.material.SetColor("_Color", Color.yellow);
         }
 
-        this.transform.LookAt(target.transform.position);
+        transform.LookAt(_targetPos);
     }
 
     IEnumerator Rotate_Around_Randomly()
     {
         int randomized_int = UnityEngine.Random.Range(-1, 1);
         yield return new WaitForSeconds(0.2f);
-        transform.RotateAround(target.transform.position, Vector3.up, speed * Time.deltaTime * randomized_int);
+        transform.RotateAround(_targetPos, Vector3.up, speed * Time.deltaTime * randomized_int);
     }
-
-
+    
     IEnumerator Go_Towards_Backwards_Randomly()
     {
         int randomized_int = UnityEngine.Random.Range(-1, 1);
@@ -107,7 +104,7 @@ public class GhostController : MonoBehaviour
         yield return new WaitForSeconds(0.2f);
         if (randomized_int < 5)
         {
-            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed / 10 * Time.deltaTime * movement_randomizer_towards);
+            transform.position = Vector3.MoveTowards(transform.position, _targetPos, speed / 10 * Time.deltaTime * movement_randomizer_towards);
 
         }
     }
@@ -116,6 +113,6 @@ public class GhostController : MonoBehaviour
     {
         int X_Randomized = UnityEngine.Random.Range(5, 15);
         int Z_Randomized = UnityEngine.Random.Range(5, 15);
-        return (new Vector3(target.transform.position.x + X_Randomized, target.transform.position.y - 1, target.transform.position.z + Z_Randomized));
+        return (new Vector3(_targetPos.x + X_Randomized, _targetPos.y - 1, _targetPos.z + Z_Randomized));
     }
 }
