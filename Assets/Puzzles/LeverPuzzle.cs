2419221555685130b8e4ef205935632013bf4a04
MariamKhakoo <48660043+MariamKhakoo@users.noreply.github.com>
2023-03-05 23:43:36 +0000

    code implemented
    needs testing 
    Enis to add banishing systems

--- Assets/Puzzles/LeverPuzzle.cs
+++ Assets/Puzzles/LeverPuzzle.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
@@ -44,4 +44,66 @@ public class LeverPuzzle : Puzzle
             lever.OnLeverStateChanged += CheckPuzzleComplete;
         }
     }
+}*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverPuzzle : Puzzle
+{
+    [SerializeField, Header("Lever object references")]
+    private List<Lever> levers = new List<Lever>();
+
+    [SerializeField, Header("The correct solution for the levers")]
+    private List<LeverState> solution = new List<LeverState>() { LeverState.Up, LeverState.Down, LeverState.Middle, LeverState.Middle, LeverState.Down };
+
+    void Start()
+    {
+        // Set initial state of each lever to be in the middle
+        foreach (var lever in levers)
+        {
+            lever.SetState(LeverState.Middle);
+        }
+
+        // Set the state of each lever to the desired solution state
+        levers[0].SetState(LeverState.Up);
+        levers[1].SetState(LeverState.Down);
+        levers[2].SetState(LeverState.Middle);
+        levers[3].SetState(LeverState.Middle);
+        levers[4].SetState(LeverState.Down);
+    }
+
+    public void CheckPuzzleComplete()
+    {
+        bool correctOrder = true;
+        bool correctState = true;
+
+        // Check if levers are in correct order and state
+        for (int i = 0; i < solution.Count; i++)
+        {
+            if (levers[i].state != solution[i])
+            {
+                correctState = false;
+            }
+
+            if (i > 0 && solution[i] < solution[i - 1])
+            {
+                correctOrder = false;
+            }
+        }
+
+        if (correctOrder && correctState)
+        {
+            Complete();
+        }
+    }
+
+    private void OnDisable()
+    {
+        foreach (var lever in levers)
+        {
+            lever.OnLeverStateChanged -= CheckPuzzleComplete;
+        }
+    }
 }
+
