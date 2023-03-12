d8776e41be1d9ce03031168c28022275c85e44c5
MariamKhakoo <48660043+MariamKhakoo@users.noreply.github.com>
2023-03-12 17:49:14 +0000

    fixed issues and added lever puzzle as prefab

--- Assets/Puzzles/LeverPuzzle.cs
+++ Assets/Puzzles/LeverPuzzle.cs
@@ -1,50 +1,3 @@
-/*using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using System.Linq;
-
-public class LeverPuzzle : Puzzle
-{
-    [SerializeField, Header("Lever object references")]
-    private List<Lever> levers = new List<Lever>();
-
-    [SerializeField, Header("The correct solution for the levers")]
-    private List<LeverState> solution = new List<LeverState>();
-
-    void Start()
-    {
-        foreach (var lever in levers)
-        {
-            lever.OnLeverStateChanged += CheckPuzzleComplete;
-        }
-    }
-
-    public void CheckPuzzleComplete()
-    {
-        int correctAnswers = 0;
-
-        for (int i = 0; i < solution.Count; i++)
-        {
-            if (solution[i] == levers[i].state)
-            {
-                correctAnswers++;
-            }
-        }
-
-        if (correctAnswers == solution.Count)
-        {
-            Complete();
-        }
-    }
-
-    private void OnDisable()
-    {
-        foreach (var lever in levers)
-        {
-            lever.OnLeverStateChanged += CheckPuzzleComplete;
-        }
-    }
-}*/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -59,6 +12,11 @@ public class LeverPuzzle : Puzzle
 
     void Start()
     {
+        foreach (var lever in levers)
+        {
+            lever.OnLeverStateChanged += CheckPuzzleComplete;
+        }
+
         // Set initial state of each lever to be in the middle
         foreach (var lever in levers)
         {
@@ -106,4 +64,3 @@ public class LeverPuzzle : Puzzle
         }
     }
 }
-
