6c70a15e35a9d19c300818897f0cacef788d8070
MariamKhakoo <48660043+MariamKhakoo@users.noreply.github.com>
2023-03-17 17:16:13 +0000

    added 3 solutions, added as a prefab and added spotlights to make it look better

--- Assets/Puzzles/LeverPuzzle.cs
+++ Assets/Puzzles/LeverPuzzle.cs
@@ -1,4 +1,51 @@
-using System.Collections;
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class LeverPuzzle : Puzzle
+{
+    [SerializeField, Header("Lever object references")]
+    private List<Lever> levers = new List<Lever>();
+
+    [SerializeField, Header("The correct solution for the levers")]
+    private List<LeverState> solution = new List<LeverState>();
+
+    void Start()
+    {
+        foreach (var lever in levers)
+        {
+            lever.OnLeverStateChanged += CheckPuzzleComplete;
+        }
+    }
+
+    public void CheckPuzzleComplete()
+    {
+        int correctAnswers = 0;
+
+        for (int i = 0; i < solution.Count; i++)
+        {
+            if (solution[i] == levers[i].state)
+            {
+                correctAnswers++;
+            }
+        }
+
+        if (correctAnswers == solution.Count)
+        {
+            Complete();
+        }
+    }
+
+    private void OnDisable()
+    {
+        foreach (var lever in levers)
+        {
+            lever.OnLeverStateChanged += CheckPuzzleComplete;
+        }
+    }
+}*/
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -63,4 +110,182 @@ public class LeverPuzzle : Puzzle
             lever.OnLeverStateChanged -= CheckPuzzleComplete;
         }
     }
+}*/
+/*public class TestScript: MonoBehaviour
+{
+    public List<LeverSolution> possibleSolutions = new List<LeverSolution>();
+}
+[System.Serializable]
+public class LeverSolution
+{
+    public List<LeverState> leverStates;
+}*/
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class LeverPuzzle : Puzzle
+{
+    [SerializeField, Header("Lever object references")]
+    private List<Lever> levers = new List<Lever>();
+
+    [SerializeField, Header("The correct solution for the levers")]
+    private List<LeverState> solution = new List<LeverState>();
+
+    [SerializeField, Header("Possible solutions for the levers")]
+    private List<LeverSolution> possibleSolutions = new List<LeverSolution>();
+
+    void Start()
+    {
+        foreach (var lever in levers)
+        {
+            lever.OnLeverStateChanged += CheckPuzzleComplete;
+        }
+    }
+
+    public void CheckPuzzleComplete()
+    {
+        int correctAnswers = 0;
+
+        for (int i = 0; i < solution.Count; i++)
+        {
+            if (solution[i] == levers[i].state)
+            {
+                correctAnswers++;
+            }
+        }
+
+        if (correctAnswers == solution.Count)
+        {
+            Complete();
+        }
+    }
+
+    public bool CheckSolution(LeverSolution solution)
+    {
+        bool isCorrect = true;
+
+        for (int i = 0; i < levers.Count; i++)
+        {
+            if (levers[i].state != solution.leverStates[i])
+            {
+                isCorrect = false;
+                break;
+            }
+        }
+
+        return isCorrect;
+    }
+
+    public void RandomizeSolution()
+    {
+        if (possibleSolutions.Count > 0)
+        {
+            int index = Random.Range(0, possibleSolutions.Count);
+            solution = possibleSolutions[index].leverStates;
+        }
+    }
+
+    private void OnDisable()
+    {
+        foreach (var lever in levers)
+        {
+            lever.OnLeverStateChanged += CheckPuzzleComplete;
+        }
+    }
+}
+
+[System.Serializable]
+public class LeverSolution
+{
+    public List<LeverState> leverStates;
+}
+
+public class TestScript : MonoBehaviour
+{
+    public List<LeverSolution> possibleSolutions = new List<LeverSolution>();
+}*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class LeverPuzzle : Puzzle
+{
+    [SerializeField, Header("Lever object references")]
+    private List<Lever> levers = new List<Lever>();
+
+    [SerializeField, Header("Possible solutions for the levers")]
+    private List<LeverSolution> possibleSolutions = new List<LeverSolution>();
+
+    void Start()
+    {
+        foreach (var lever in levers)
+        {
+            lever.OnLeverStateChanged += CheckPuzzleComplete;
+        }
+    }
+
+    public void CheckPuzzleComplete()
+    {
+        int correctAnswers = 0;
+
+        for (int i = 0; i < possibleSolutions[0].leverStates.Count; i++)
+        {
+            if (possibleSolutions[0].leverStates[i] == levers[i].state)
+            {
+                correctAnswers++;
+            }
+        }
+
+        if (correctAnswers == possibleSolutions[0].leverStates.Count)
+        {
+            Complete();
+        }
+    }
+
+    public bool CheckSolution(LeverSolution solution)
+    {
+        bool isCorrect = true;
+
+        for (int i = 0; i < levers.Count; i++)
+        {
+            if (levers[i].state != solution.leverStates[i])
+            {
+                isCorrect = false;
+                break;
+            }
+        }
+
+        return isCorrect;
+    }
+
+    public void RandomizeSolution()
+    {
+        if (possibleSolutions.Count > 0)
+        {
+            int index = Random.Range(0, possibleSolutions.Count);
+            possibleSolutions[0] = possibleSolutions[index];
+        }
+    }
+
+    private void OnDisable()
+    {
+        foreach (var lever in levers)
+        {
+            lever.OnLeverStateChanged += CheckPuzzleComplete;
+        }
+    }
+}
+
+[System.Serializable]
+public class LeverSolution
+{
+    public List<LeverState> leverStates;
+}
+
+public class TestScript : MonoBehaviour
+{
+    public List<LeverSolution> possibleSolutions = new List<LeverSolution>();
 }
